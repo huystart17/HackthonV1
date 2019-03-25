b705f1e9dc6761409afb3a5ecb0546410db87a08
huyhuy17-site <huynguyenquang@enbac.com>
2019-03-25 07:32:31 +0700

    cappj nhật

--- mc.py
+++ mc.py
@@ -3,9 +3,12 @@ mc = False
 server_online = "103.237.147.88"
 server_local = "localhost"
 
+port_lvl5 = 4712
+port_luyen_thi = 4713
+
 while True:
     try:
-        mc = minecraft.Minecraft.create(server_online)
+        mc = minecraft.Minecraft.create(server_local, 4711)
         break
     except:
         mc = False
