6651e8ef2bdc54916df6ab8290548615f91525ce
huyhuy17-site <huynguyenquang@enbac.com>
2019-04-05 21:39:42 +0700

    cappj nhật

--- main.py
+++ main.py
@@ -2,6 +2,3 @@ from City import *
 from threading import Thread
 # ct.clear()
 # ct.clear()
-ct.load('treefly', '2')
-ct.teleport_all()
-ct.monitor()
--- mc.py
+++ mc.py
@@ -5,10 +5,13 @@ server_local = "localhost"
 
 port_lvl5 = 4712
 port_luyen_thi = 4713
-port_may_bay = 4714
+port_may_bay = 4710
+#sanbay bay {"x": -2612, "y": 96, "z": -99}
+#in game
+port_may_bay_game = 25560
 while True:
     try:
-        mc = minecraft.Minecraft.create(server_local, port_may_bay)
+        mc = minecraft.Minecraft.create(server_online, port_lvl5)
         break
     except:
         mc = False
